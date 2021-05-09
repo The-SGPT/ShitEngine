0e0e3c41f78d068d4892b5cdb5f7af37b44d0f2d
/nick haya <74699483+The-SGPT@users.noreply.github.com>
2021-05-09 16:13:19 +0800

    WRONG ASS MOD CHART LUA ARGHH

--- assets/preload/data/ass/modchart.lua
+++ assets/preload/data/ass/modchart.lua
@@ -3,25 +3,13 @@ function start(song) -- do nothing
 end
 
 function update(elapsed)
-    if difficulty == 2 and curStep > 400 then
         local currentBeat = (songPos / 1000)*(bpm/60)
 		for i=0,7 do
 			setActorX(_G['defaultStrum'..i..'X'] + 32 * math.sin((currentBeat + i*0.25) * math.pi), i)
 			setActorY(_G['defaultStrum'..i..'Y'] + 32 * math.cos((currentBeat + i*0.25) * math.pi), i)
+			setActorSkewX(32 * math.sin((currentBeat + i*0) * math.pi), i)
+			setNoteSkewX(-32 * math.sin((currentBeat + i*0) * math.pi), i)
 		end
-    end
-
-   for i=0,7 do
-   	setActorScaleX(lerp(0.7, 0.7, 0.50), i)
-   	setNoteScaleXY(lerp(0.7, 0.7, 0.50), 0.7, i)
-   end
-
-   if (mod(curBeat,1) == 0) then
-		for i=0,7 do
-			setActorScaleX(2, i)
-			setNoteScaleXY(2, 0.7, i)
-		end
-   end
 end
 
 function beatHit(beat) -- do nothing
